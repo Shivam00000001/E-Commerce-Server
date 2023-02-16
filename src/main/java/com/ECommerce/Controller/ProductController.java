353a932812db4c99695a851fb53d9bf33f0e6a5c
Shivam Sinha <shivams.sinha0@gmail.com>
2023-02-16 13:01:01 +0530

    Added Upload Images

--- src/main/java/com/ECommerce/Controller/ProductController.java
+++ src/main/java/com/ECommerce/Controller/ProductController.java
@@ -1,10 +1,17 @@
 package com.ECommerce.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ECommerce.Entity.ImageModel;
 import com.ECommerce.Entity.Product;
 import com.ECommerce.Service.ProductService;
 
@@ -14,8 +21,25 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
-	@PostMapping("/addNewProduct")
-	public Product addNewProduct(@RequestBody Product product) {
+	@PostMapping(value = "/addNewProduct", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+	public Product addNewProduct(@RequestPart("product") Product product, @RequestPart("imageFile")MultipartFile[] file) {
+	
+		try {
+			Set<ImageModel>images = uploadImage(file);
+			product.setProductImages(images);
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 		return productService.addNewProduct(product);
+	}
+	
+	public Set<ImageModel> uploadImage(MultipartFile[] multipartFiles) throws IOException {
+		Set<ImageModel> imageModels = new HashSet<>();
+		for(MultipartFile file: multipartFiles) {
+			ImageModel imageModel = new ImageModel(file.getOriginalFilename(),file.getContentType(),file.getBytes());
+			imageModels.add(imageModel);
+		}
+		return imageModels;
 	}
 }
